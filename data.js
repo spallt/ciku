7807980d47466b80fded4ad021717e414b3c27a5
spallt <128279230+spallt@users.noreply.github.com>
2025-07-15 14:34:29 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,2 +1,6 @@
 window.PASTEBIN_CONTENT = "https://pastebin.com/raw/rrrEYzPZ";
 window.JSONBIN_CONTENT = "https://api.jsonbin.io/v3/b/6875ebd9b71c833519b09c2e/raw";
+window.pastebin_CONTENT = "https://pastebin.com/raw/rrrEYzPZ";
+window.jsonbin_CONTENT = "https://api.jsonbin.io/v3/b/6875ebd9b71c833519b09c2e/raw";
+window.pastebin = "https://pastebin.com/raw/rrrEYzPZ";
+window.jsonbin = "https://api.jsonbin.io/v3/b/6875ebd9b71c833519b09c2e/raw";
